f5b9c4a4989109e591feae4be11207ac4e990ff0
Jacky Wu <Jackyw1@uci.edu>
2021-04-18 16:09:36 -0700

    test linter

--- server/Model/model.js
+++ server/Model/model.js
@@ -4,6 +4,7 @@ const { TOKEN } = require('../../config.js');
 module.exports = {
   getAll: (endpoint) => {
     let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo${endpoint}`;
+    console.log('testing linter in model.js');
 
     return axios({
       url: url,
