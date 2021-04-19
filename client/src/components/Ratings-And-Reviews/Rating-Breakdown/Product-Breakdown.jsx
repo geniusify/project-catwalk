8c8337b9a0dbe25e480016d1c20356cad873c39e
Jacky Wu <Jackyw1@uci.edu>
2021-04-19 14:22:43 -0700

    Add template for Rating Summary component

---
+++ client/src/components/Ratings-And-Reviews/Rating-Breakdown/Product-Breakdown.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ProductBreakdown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        test rendering product breakdown
+      </div>
+    );
+  }
+}
+
+export default ProductBreakdown;
