8f178fa3334a21f5da811d938e0814abd67b0968
Eric Vogler <ericvogler@gmail.com>
2021-04-20 11:44:07 -0700

    Create cart form elements

--- client/src/components/overview/AddToCart.jsx
+++ client/src/components/overview/AddToCart.jsx
@@ -1,12 +1,27 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable semi */
 /* eslint-disable arrow-body-style */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-const AddToCart = (props) => {
+const AddToCart = ({ style }) => {
+  const availableSizes = Object.values(style.skus)
+    .filter((sku) => sku.quantity > 0)
+    .map((sku) => sku.size);
+
   return (
     <div className="ov-add-to-cart">
-      <div>SELECT SIZE.............QUANTITY</div>
-      AddToCart
+      {availableSizes.join(', ')}
+      <form>
+        <select className="ov-cart-size" name="size">
+          {availableSizes.map((size) => (
+            <option value="{size}">{size}</option>
+          ))}
+        </select>
+        <select className="ov-cart-quantity" name="quantity">1</select>
+        <button className="ov-cart-add" type="button">Add To Bag</button>
+        <button className="ov-cart-like" type="button">*</button>
+      </form>
     </div>
   );
 };
