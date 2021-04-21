8cf7629a6d55af4a8e8df785a6f675904ee6e3ba
Eric Vogler <ericvogler@gmail.com>
2021-04-21 08:54:27 -0700

    Create css classes for gallery thumbnails

--- client/src/components/overview/GalleryThumbnails.jsx
+++ client/src/components/overview/GalleryThumbnails.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const GalleryThumbnails = ({ photos }) => {
-  return (
-    <div>thumbnails</div>
-  );
-}
+const GalleryThumbnails = ({ photos }) => (
+  <div className="ov-gallery-thumbnails">
+    {photos.map((photo) => (
+      <div
+        key={photo.url}
+        className="ov-gallery-thumbnail"
+        style={{ backgroundImage: `url(${photo.thumbnail_url})` }}
+      />
+    ))}
+
+  </div>
+);
 
 export default GalleryThumbnails;
