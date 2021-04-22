8eb8c3c4ef18f40c3350f83805b6c0f91aede605
Eric Vogler <ericvogler@gmail.com>
2021-04-21 19:21:26 -0700

    Make gallery thumbnails clickable

--- client/src/components/overview/GalleryThumbnails.jsx
+++ client/src/components/overview/GalleryThumbnails.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const GalleryThumbnails = ({ photos }) => (
+const GalleryThumbnails = ({ photos, chooseThumbnail, imageIndex }) => (
   <div className="ov-gallery-thumbnails">
-    {photos.map((photo) => (
+    {photos.map((photo, index) => (
       <div
         key={photo.url}
-        className="ov-gallery-thumbnail"
+        className={`ov-gallery-thumbnail ${index === imageIndex ? 'ov-gallery-thumbnail-selected' : ''}`}
+        onClick={() => chooseThumbnail(index)}
         style={{ backgroundImage: `url(${photo.thumbnail_url})` }}
       />
     ))}
--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import GalleryThumbnails from './GalleryThumbnails.jsx';
 
 const ImageGallery = ({ photos, alt }) => {
-  const a = 123;
+  const [imageIndex, setImageIndex] = useState(0);
   return (
     <div className="ov-image-gallery">
-      <img className="ov-main-gallery-image" src={photos[0].url} alt={alt} />
-      <GalleryThumbnails photos={photos} />
+      <img className="ov-main-gallery-image" src={photos[imageIndex].url} alt={alt} />
+      <GalleryThumbnails
+        photos={photos}
+        chooseThumbnail={setImageIndex}
+        imageIndex={imageIndex}
+      />
     </div>
   );
 };
