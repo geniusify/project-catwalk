2114cfaa4df2cae41eb5edeaf38717b8a7f96eb5
Eric Vogler <ericvogler@gmail.com>
2021-04-21 19:44:30 -0700

    Clean up Overveiw.jsx

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -31,7 +31,6 @@ const Overview = ({ productId }) => {
   const [styles, setStyles] = useState(undefined);
   const [meta, setMeta] = useState(undefined);
   const [styleIndex, setStyleIndex] = useState(0);
-  // const [defaultStyleIndex, setDefaultStyleIndex] = useState(undefined);
   const [style, setStyle] = useState(undefined);
 
   const updateStyleIndex = (idx) => {
@@ -41,15 +40,10 @@ const Overview = ({ productId }) => {
 
   useEffect(() => {
     if (!info) {
-      // if (true) {
       axios({ url: `api/products/${productId}`, method: 'get' })
-        .then((res) => {
-          setInfo(res.data);
-          // console.log('info:', res.data);
-        })
+        .then((res) => { setInfo(res.data); })
         .catch((err) => console.log('failed retrieving data', err));
     }
-
     if (!styles) {
       axios({ url: `api/products/${productId}/styles`, method: 'get' })
         .then((res) => {
@@ -61,45 +55,18 @@ const Overview = ({ productId }) => {
         })
         .catch((err) => console.log('failed retrieving data', err));
     }
-
     if (!meta) {
       axios({ url: `api/reviews/meta?product_id=${productId}`, method: 'get' })
-        .then((res) => {
-          setMeta(res.data);
-        })
+        .then((res) => { setMeta(res.data); })
         .catch(() => console.log('failed retrieving data'));
     }
   }, []);
 
-  // XXX: initial style is actually found in styles data
-
   const readyToRender = !!(!!info && !!style && !!styles && !!meta);
-
-  console.log('readyToRender:', readyToRender);
-
-  let rendering = 'fail';
-
-  // if (readyToRender) {
-  //   console.log('style as of render', JSON.stringify(style));
-  //   console.log('styles as of render', JSON.stringify(styles));
-  // }
-
+  // console.log('readyToRender:', readyToRender);
+  let rendering = 'unrendered';
   try {
     rendering = readyToRender ?
-      // ? (
-      //   <div>
-      //     loaded!
-      //     <div>INFO: {JSON.stringify(info)}</div><br />
-      //     <div>STYLES: {JSON.stringify(styles)}</div><br />
-      //     <div>META: {JSON.stringify(meta)}</div><br />
-      //     <div>STYLE: {JSON.stringify(style)}</div><br />
-      //     <div>PRICE: {style.original_price}</div><br />
-      //     <div>SALEPRICE: {style.sale_price}</div><br />
-      //     {/* <div>defaultStyleIndex: {defaultStyleIndex}</div> */}
-      //     <div>styleIndex: {styleIndex}</div><br />
-      //     <div>FEATURES: {JSON.stringify(info.features)}</div><br />
-      //   </div>
-      // )
       (
         <div className="ov-container">
           <ImageGallery photos={style.photos} alt={info.name} />
