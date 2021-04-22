feeb7a3063fbfc0aa26dbd694a29f4c4bed1cb36
Eric Vogler <ericvogler@gmail.com>
2021-04-22 09:24:54 -0700

    Make thumbnail scroller stop at top and bottom

--- client/src/components/overview/GalleryThumbnails.jsx
+++ client/src/components/overview/GalleryThumbnails.jsx
@@ -4,12 +4,18 @@ import React, { useState } from 'react';
 const GalleryThumbnails = ({ photos, chooseThumbnail, imageIndex }) => {
   const [topIndex, setTopIndex] = useState(0);
   const thumbOffset = -90;
-
+  const height = Math.min(photos.length, 7) * 90 - 30 + 4;
+  const canScrollUp = topIndex > 0;
+  const canScrollDown = photos.length - topIndex > 7;
   return (
-    <div className="ov-gallery-thumbnail-viewer">
-      <img className="ov-up-arrow" src="icons/up.svg" onClick={()=>setTopIndex((i) => i - 1)} />
-      <div className="ov-gallery-thumbnails-container">
-        <div className="ov-gallery-thumbnails" style={{ top: topIndex * thumbOffset }}>
+    <div className="ov-gallery-thumbnail-viewer"
+      style={{ gridTemplateRows: `50px ${height}px 50px` }}>
+      {canScrollUp
+        ? (<img className="ov-up-arrow" src="icons/up.svg" onClick={() => setTopIndex((i) => i - 1)} />)
+        : null}
+      <div className="ov-gallery-thumbnails-container"
+        style={{ height }}>
+        <div className="ov-gallery-thumbnails" style={{ top: topIndex * thumbOffset + 3 }}>
           {photos.map((photo, index) => (
             <div
               key={photo.url}
@@ -20,7 +26,9 @@ const GalleryThumbnails = ({ photos, chooseThumbnail, imageIndex }) => {
           ))}
         </div>
       </div>
-      <img className="ov-down-arrow" src="icons/down.svg" onClick={()=>setTopIndex((i) => i + 1)} />
+      {canScrollDown
+        ? (<img className="ov-down-arrow" src="icons/down.svg" onClick={() => setTopIndex((i) => i + 1)} />)
+        : null}
     </div>
   )
 };
