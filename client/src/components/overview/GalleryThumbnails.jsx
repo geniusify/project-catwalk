9fbf034ad0b5f752f5cd2c350607047e8888a090
Eric Vogler <ericvogler@gmail.com>
2021-04-22 09:04:44 -0700

    Make thumbnails scrollable; Add arrow icons

--- client/src/components/overview/GalleryThumbnails.jsx
+++ client/src/components/overview/GalleryThumbnails.jsx
@@ -1,19 +1,28 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 
-const GalleryThumbnails = ({ photos, chooseThumbnail, imageIndex }) => (
-  <div className="ov-gallery-thumbnails">
-    <span className="ov-up-arrow">^</span>
-    {photos.map((photo, index) => (
-      <div
-        key={photo.url}
-        className={`ov-gallery-thumbnail ${index === imageIndex ? 'ov-gallery-thumbnail-selected' : ''}`}
-        onClick={() => chooseThumbnail(index)}
-        style={{ backgroundImage: `url(${photo.thumbnail_url})` }}
-      />
-    ))}
-    <span className="ov-down-arrow">^</span>
-  </div>
-);
+const GalleryThumbnails = ({ photos, chooseThumbnail, imageIndex }) => {
+  const [topIndex, setTopIndex] = useState(0);
+  const thumbOffset = -90;
+
+  return (
+    <div className="ov-gallery-thumbnail-viewer">
+      <img className="ov-up-arrow" src="icons/up.svg" onClick={()=>setTopIndex((i) => i - 1)} />
+      <div className="ov-gallery-thumbnails-container">
+        <div className="ov-gallery-thumbnails" style={{ top: topIndex * thumbOffset }}>
+          {photos.map((photo, index) => (
+            <div
+              key={photo.url}
+              className={`ov-gallery-thumbnail ${index === imageIndex ? 'ov-gallery-thumbnail-selected' : ''}`}
+              onClick={() => chooseThumbnail(index)}
+              style={{ backgroundImage: `url(${photo.thumbnail_url})` }}
+            />
+          ))}
+        </div>
+      </div>
+      <img className="ov-down-arrow" src="icons/down.svg" onClick={()=>setTopIndex((i) => i + 1)} />
+    </div>
+  )
+};
 
 export default GalleryThumbnails;
