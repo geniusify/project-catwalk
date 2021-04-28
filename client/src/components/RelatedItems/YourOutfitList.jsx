cb939ce097a16b842a7b64024fd47c0c8b06b7d8
Yasi <khorramyasaman@gmail.com>
2021-04-28 08:14:51 -0700

    make small css changes

--- client/src/components/RelatedItems/YourOutfitList.jsx
+++ client/src/components/RelatedItems/YourOutfitList.jsx
@@ -3,18 +3,11 @@ import React from 'react';
 const YourOutfitList = () => {
   return (
     <div className='carouselInnerDiv'>
-
       <button className='outfitButton'>
         +
-
-
         </button>
-
         <p className="outfitRelated">Add to Outfit</p>
-
-
     </div>
-
   )
 }
 
